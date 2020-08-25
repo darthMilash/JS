eab7c1a353fee0089984065a8b8f8bff9ea9e267
darthMilash <darthmilash@gmail.com>
2020-08-25 23:36:53 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,95 @@
+  "use strict";
+
+                        // 1- LESSON        
+
+//   let number = 5;
+//   const leftBorderWidth = 1;
+
+//   number = 10;
+//   console.log(number);
+
+//   const obj = {
+//       a: 50
+//   };
+
+//   obj.a = 10;
+
+//   console.log(obj);
+
+
+//                         2- LESSON            
+// let number = 4.6;
+
+// console.log(4/0);
+// console.log('string' * 9);
+
+// const persone = 'Alex';
+
+// const bool = true;
+
+// console.log(something);
+
+// let und;
+// console.log(und);
+
+// const obj = {
+//     name: "Jack",
+//     age: 25, 
+//     isMarried: false
+// };
+
+// console.log(obj.name);
+
+// let arr = ['plum.png', 'orange.jpg', 6, 'apple.bmp', {}, []];
+// console.log(arr[0]);
+
+
+                        // 3- LESSON       
+
+// // const result = confirm("Are you here?");
+// // console.log(result);
+
+// // const answer = +prompt('Do you have 18', '18');
+// // console.log(answer+5);
+
+// const answers = [];
+
+// answers[0] = prompt('What is your name', '');
+// answers[1] = prompt('What is your second name', '');
+// answers[2] = prompt('How old?', '');
+
+// console.log(typeof(answers));
+
+                        // 4- LESSON
+
+//   const category = 'toys';
+
+//   console.log(`https://someurl.com/${category}/5`);
+
+//   const user = 'Ivan';
+
+//   alert(`Hello, ${user}`);
+
+                        // 5- LESSON
+  console.log(4+ +"5");
+  
+  let incr = 10,
+      decr = 10;
+
+//   incr++;
+//   decr--;
+
+  console.log(++incr);
+  console.log(--decr);
+
+  console.log(5%2);
+
+  console.log(2*4 === 8);
+
+  const isChecked = true,
+        isClose = false;
+
+  console.log(isChecked && isClose);
+  console.log(isChecked || !isClose);
+
+
