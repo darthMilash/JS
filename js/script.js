7286ab1c7931fbc4fb62f42e975443afee1a0f09
darthMilash <darthmilash@gmail.com>
2020-09-15 23:10:35 +0300

    Lesson 22

--- js/script.js
+++ js/script.js
@@ -308,22 +308,104 @@
 
 // LESSON 22 
 
-let a = 5,
-b =a;
+// let a = 5,
+// b =a;
 
-b = b + 5;
+// b = b + 5;
 
-console.log(b); 
-console.log(a);
+// console.log(b); 
+// console.log(a);
 
-const ob = {
-      a: 5,
-      b: 1
-};
+// const ob = {
+//       a: 5,
+//       b: 1
+// };
 
-const copy = ob;
+// const copy = ob; //link
 
-copy.a = 10;
+// copy.a = 10;
 
-console.log(copy);
- console.log(ob);
+// console.log(copy);
+//  console.log(ob);
+
+// // 1
+
+//  function copy2(mainObj) {
+//     let objCopy = {};
+
+//     let key;
+//     for (key in mainObj) {
+//       objCopy[key] = mainObj[key];
+//     }
+//     return objCopy;
+//  }
+
+//  const numbers = { 
+//    a: 2,
+//    b: 5,
+//    c: {
+//      x: 7,
+//      y: 4
+//    }
+//  };
+
+
+//  const newNumbers = copy2(numbers);
+
+//  newNumbers.a = 10;
+//  newNumbers.c.x = 10;
+
+//  console.log(newNumbers);
+//  console.log(numbers);
+
+//  const add = {
+//    d: 17,
+//    e: 20
+//  };
+
+// //  2
+
+//  const clone = Object.assign({}, add);
+
+//  clone.d = 20;
+
+//  console.log(add);
+//  console.log(clone);
+
+
+//  const oldArray = [`a`, `b`, `c`];
+//  const newArray = oldArray.slice();
+
+//  newArray[1] = `adada`;
+//  console.log(newArray);
+//  console.log(oldArray);
+
+// // 3
+
+//  const video = [`720p`, `1024p`, `2k`],
+//        speed = [`1.25`, `1.5`, `2.0`],
+//        internet = [...video, ...speed, `vk`, `fcb`];
+// console.log(internet);
+
+// function log(a, b, c) {
+//   console.log(a);
+//   console.log(b);
+//   console.log(c);
+// }
+
+// const num = [2, 5, 7];
+
+// log(...num);
+
+// // 4
+
+// const array = [`a`, `b`];
+
+// const newArray2 = [...array];
+
+// const q = {
+//   one: 1,
+//   two: 2
+// };
+
+// const newQ = {...q};
