65c1f70e12ae9fa834c607771190e9ef70ddd6d2
darthMilash <darthmilash@gmail.com>
2020-10-07 23:34:13 +0300

    Add 28 lesson

--- js/script.js
+++ js/script.js
@@ -437,49 +437,51 @@
 //To string
 
 //1) 
-console.log(typeof(String(null)));
-console.log(typeof(String(4)));
+// console.log(typeof(String(null)));
+// console.log(typeof(String(4)));
 
-//2)
-console.log(typeof(5 + '1'));
+// //2)
+// console.log(typeof(5 + '1'));
 
-const num = 5;
-console.log("https://vk.com/catalog/" + num);
+// const num = 5;
+// console.log("https://vk.com/catalog/" + num);
 
-const fontSize = 26 + 'px';
+// const fontSize = 26 + 'px';
 
-// To Number
+// // To Number
 
-//1) 
-console.log(typeof(Number('4')));
+// //1) 
+// console.log(typeof(Number('4')));
 
-//2)
-console.log(typeof(+'5'));
+// //2)
+// console.log(typeof(+'5'));
 
-//3)
-console.log(typeof(parseInt("15px", 10)));
+// //3)
+// console.log(typeof(parseInt("15px", 10)));
 
-let answer = +prompt("Hello", "");
+// let answer = +prompt("Hello", "");
 
-//To boolean
+// //To boolean
 
-//false 0, '', null, undefined, NaN;
+// //false 0, '', null, undefined, NaN;
 
-//1
-let switcher = null;
+// //1
+// let switcher = null;
 
-if (switcher) {
-  console.log("Working...");
-}
+// if (switcher) {
+//   console.log("Working...");
+// }
 
-switcher = 1;
+// switcher = 1;
 
-if (switcher) {
-  console.log("Working...");
-}
+// if (switcher) {
+//   console.log("Working...");
+// }
 
-//2
-console.log(typeof(Boolean('4')));
+// //2
+// console.log(typeof(Boolean('4')));
 
-//3
-console.log(typeof(!!'4'));
+// //3
+// console.log(typeof(!!'4'));
+
+// LESSON 28 in folder
